89a2b45d63ff21bda455a26e1533a3fa39e5e1e0
liyupi <592789970@qq.com>
2023-09-26 11:41:58 +0800

    直播后的补充
    add 项目支持微服务部署

--- yuoj-backend-judge-service/src/main/java/com/yupi/yuojbackendjudgeservice/YuojBackendJudgeServiceApplication.java
+++ yuoj-backend-judge-service/src/main/java/com/yupi/yuojbackendjudgeservice/YuojBackendJudgeServiceApplication.java
@@ -18,8 +18,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class YuojBackendJudgeServiceApplication {
 
     public static void main(String[] args) {
-        // 初始化消息队列
-        InitRabbitMq.doInit();
+        // 初始化消息队列，先注释掉，改用 Bean 的方式初始化消息队列（InitRabbitMqBean.java）
+//        InitRabbitMq.doInit();
         SpringApplication.run(YuojBackendJudgeServiceApplication.class, args);
     }
 
